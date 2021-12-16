0dad1cb3c45b609c28339d8d5c395985000fe82e
Pepe Fabra Valverde <jofaval@outlook.com>
2021-12-16 14:00:40 +0100

    Fix class naming

--- src/entity/metaHero.py
+++ src/entity/metaHero.py
@@ -7,7 +7,7 @@ class metaHero(type):
 
     __instance = None
 
-    def __call__(self, *args, **kwargs):
-        if self.__instance is None: self.__instance = super(metaHero, cls).__call__(*args, **kwargs)
+    def __call__(cls, *args, **kwargs):
+        if cls.__instance is None: cls.__instance = super(metaHero, cls).__call__(*args, **kwargs)
 
-        return self.__instance
+        return cls.__instance
