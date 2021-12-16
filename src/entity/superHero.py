404b08c5b9de3e9e14424cab6052559d6f45ae45
ricardosorli <ricardosorli@gmail.com>
2021-12-16 13:57:07 +0100

    Test local ok 1

--- src/entity/superHero.py
+++ src/entity/superHero.py
@@ -5,3 +5,17 @@ class superHero(metaclass=metaHero):
     Any singleton should define some business logic, which can be
     executed on its instance.
     """
+
+
+    def __init__(self):
+        self.name = 'Batman'
+        self.power = 1024
+        self.secretName = 'Bruce Wayne'
+        self.city = 'Gotham'
+        self.location = 'Batcave'
+
+    def maxPower(self, increase):
+        self.power = increase
+
+
+
