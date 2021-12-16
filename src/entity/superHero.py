c0238e1a97f544c5822e0b8db951e491e8546822
Fabrizio Pironi <fabriziopironi@gmail.com>
2021-12-16 13:29:04 +0100

    Reto 2 lsito

--- src/entity/superHero.py
+++ src/entity/superHero.py
@@ -5,3 +5,16 @@ class superHero(metaclass=metaHero):
     Any singleton should define some business logic, which can be
     executed on its instance.
     """
+    def __init__(self):
+        self.name = 'Batman'
+        self.power = 1024
+        self.secretName = 'Bruce Wayne'
+        self.city = 'Gotham'
+        self.location = 'Batcave'
+
+    def maxPower(self, newPower):
+        if newPower < 1024:
+            print('maxPower has to be >= 1024')
+        else:
+            self.power = newPower
+        
